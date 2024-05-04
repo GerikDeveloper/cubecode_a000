c7c9d2d125627853632d88ce70df388b21508876
GerikDeveloper <geeerik7@gmail.com>
2024-05-04 12:20:18 +0300

    ray_get_002(renaming)

--- src/world.rs
+++ src/world.rs
@@ -4,7 +4,7 @@ use std::ops::{Deref, DerefMut};
 use thiserror::Error;
 use crate::chunk::{Chunk, ChunkGenerator};
 use crate::render::blocks_loader::{AIR_BLOCK_ID, BlocksLoader};
-use crate::render::types::{Vec3f, Vec3ub};
+use crate::render::types::{Vec3f, Vec3i, Vec3s, Vec3ub};
 
 const CHUNK_SIZE: usize = 16 * 16 * 16 * 16 * 2;
 
@@ -69,50 +69,48 @@ impl World {
     }
 
     pub fn ray_get(&self, pos: &Vec3f, dir: &Vec3f, max_dist: f32, end: &mut Vec3f, norm: &mut Vec3f, iend: &mut Vec3ub) -> Option<u16> {
-        let mut t: f32 = 0.0;
+        let mut pdist: f32 = 0.0; //passed dist
 
-        let mut ix: i32 = pos[0] as i32;
-        let mut iy: i32 = pos[1] as i32;
-        let mut iz: i32 = pos[2] as i32;
+        let mut ipos: Vec3i = [(pos[0] as i32), (pos[1] as i32), (pos[2] as i32)];
 
         let stepx: f32 = {if dir[0] > 0.0f32 { 1.0 } else { -1.0 }};
         let stepy: f32 = {if dir[1] > 0.0f32 { 1.0 } else { -1.0 }};
         let stepz: f32 = {if dir[2] > 0.0f32 { 1.0 } else { -1.0 }};
 
         /*
-        indicates how far along the ray we must move (in units of t) for the horizontal component of such a movement to equal the width of a voxel.
+        indicates how far along the ray we must move (in units of pdist) for the horizontal component of such a movement to equal the width of a voxel.
          */
         let tdx: f32 = {if dir[0] == 0.0f32 { f32::INFINITY } else { f32::abs(1.0f32 / dir[0]) }};
         let tdy: f32 = {if dir[1] == 0.0f32 { f32::INFINITY } else { f32::abs(1.0f32 / dir[1]) }};
         let tdz: f32 = {if dir[2] == 0.0f32 { f32::INFINITY } else { f32::abs(1.0f32 / dir[2]) }};
 
-        let xdist: f32 = {if stepx > 0.0f32 { (ix as f32) + 1.0f32 - pos[0] } else { pos[0] - (ix as f32) }};
-        let ydist: f32 = {if stepy > 0.0f32 { (iy as f32) + 1.0f32 - pos[1] } else { pos[1] - (iy as f32) }};
-        let zdist: f32 = {if stepz > 0.0f32 { (iz as f32) + 1.0f32 - pos[2] } else { pos[2] - (iz as f32) }};
+        let xdist: f32 = {if stepx > 0.0f32 { (ipos[0] as f32) + 1.0f32 - pos[0] } else { pos[0] - (ipos[0] as f32) }};
+        let ydist: f32 = {if stepy > 0.0f32 { (ipos[1] as f32) + 1.0f32 - pos[1] } else { pos[1] - (ipos[1] as f32) }};
+        let zdist: f32 = {if stepz > 0.0f32 { (ipos[2] as f32) + 1.0f32 - pos[2] } else { pos[2] - (ipos[2] as f32) }};
 
 
         /*
-        we determine the value of t at which the ray crosses the first vertical voxel boundary and store it in variable tMaxX.
+        we determine the value of pdist at which the ray crosses the first vertical voxel boundary and store it in variable tMaxX.
          */
-        let mut txmax: f32 = {if tdx < f32::INFINITY { tdx * xdist } else { f32::INFINITY }}; //smallest positive t such that pos[0] + t * dir[0] is an int
+        let mut txmax: f32 = {if tdx < f32::INFINITY { tdx * xdist } else { f32::INFINITY }}; //smallest positive pdist such that pos[0] + pdist * dir[0] is an int
         let mut tymax: f32 = {if tdy < f32::INFINITY { tdy * ydist } else { f32::INFINITY }};
         let mut tzmax: f32 = {if tdz < f32::INFINITY { tdz * zdist } else { f32::INFINITY }};
 
-        let mut stepind: i32 = -1;
+        let mut stepind: i8 = -1;
 
-        while t <= max_dist {
-            if ix <= 0xFF && ix >= 0x00 &&
-                iy <= 0xFF && iy >= 0x00 &&
-                iz <= 0xFF && iz >= 0x00 {
-                let block = self.get_block(&[(ix as u8), (iy as u8), (iz as u8)]);
+        while pdist <= max_dist {
+            if ipos[0] <= 0xFF && ipos[0] >= 0x00 &&
+                ipos[1] <= 0xFF && ipos[1] >= 0x00 &&
+                ipos[2] <= 0xFF && ipos[2] >= 0x00 {
+                let block = self.get_block(&[(ipos[0] as u8), (ipos[1] as u8), (ipos[2] as u8)]);
                 if block != AIR_BLOCK_ID {
-                    end[0] = pos[0] + t * dir[0];
-                    end[1] = pos[1] + t * dir[1];
-                    end[2] = pos[2] + t * dir[2];
+                    end[0] = pos[0] + pdist * dir[0];
+                    end[1] = pos[1] + pdist * dir[1];
+                    end[2] = pos[2] + pdist * dir[2];
 
-                    iend[0] = ix as u8;
-                    iend[1] = iy as u8;
-                    iend[2] = iz as u8;
+                    iend[0] = ipos[0] as u8;
+                    iend[1] = ipos[1] as u8;
+                    iend[2] = ipos[2] as u8;
 
                     norm[0] = 0.0f32;
                     norm[1] = 0.0f32;
@@ -127,25 +125,25 @@ impl World {
                 }
                 if txmax < tymax {
                     if txmax < tzmax {
-                        ix += stepx as i32;
-                        t = txmax;
+                        ipos[0] += stepx as i32;
+                        pdist = txmax;
                         txmax += tdx;
                         stepind = 0;
                     } else {
-                        iz += stepz as i32;
-                        t = tzmax;
+                        ipos[2] += stepz as i32;
+                        pdist = tzmax;
                         tzmax += tdz;
                         stepind = 2;
                     }
                 } else {
                     if tymax < tzmax {
-                        iy += stepy as i32;
-                        t = tymax;
+                        ipos[1] += stepy as i32;
+                        pdist = tymax;
                         tymax += tdy;
                         stepind = 1;
                     } else {
-                        iz += stepz as i32;
-                        t = tzmax;
+                        ipos[2] += stepz as i32;
+                        pdist = tzmax;
                         tzmax += tdz;
                         stepind = 2;
                     }
@@ -155,13 +153,13 @@ impl World {
             }
         }
 
-        iend[0] = ix as u8;
-        iend[1] = iy as u8;
-        iend[2] = iz as u8;
+        iend[0] = ipos[0] as u8;
+        iend[1] = ipos[1] as u8;
+        iend[2] = ipos[2] as u8;
 
-        end[0] = pos[0] + (t * dir[0]);
-        end[1] = pos[1] + (t * dir[1]);
-        end[2] = pos[2] + (t * dir[2]);
+        end[0] = pos[0] + (pdist * dir[0]);
+        end[1] = pos[1] + (pdist * dir[1]);
+        end[2] = pos[2] + (pdist * dir[2]);
 
         norm[0] = 0.0f32;
         norm[1] = 0.0f32;
